f20fb87d4a83f3cd0aba3233627e899cf3d7c0c3
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-06-05 12:13:25 -0400

    Update app.js
    fixed missing  '  '

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   //will make grid mean class grid and squares mean an array of all the divs of class grid
   const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div));
+  let squares = Array.from(document.querySelectorAll('.grid div'));
 
   console.log(squares);
 
