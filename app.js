76cf9c009b82a829fafcc9b39f4f94d338ec84bf
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-06-05 12:10:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // all code goes here
+  // all javascript code goes inside this
   const width = 10;
   //will make grid mean class grid and squares mean an array of all the divs of class grid
   const grid = document.querySelector('.grid');
