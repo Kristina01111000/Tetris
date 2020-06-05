6aa5f5acd7a05e3c3fd852e8b0ee971cc25d43d6
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-06-05 13:16:06 -0400

    added L tetris piece

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartButton = document.querySelector('#start-button');
 
-
+  // making the tetris pieces
+  const lpiece = [
+    [1,width+1,width*2+1,2],
+    [width,width+1,width+2,width*2+2],
+    [1,width+1,width*2+1,width*2],
+    [width, width*2,width*2+1,width*2+2]
+   ];
+  
+  
 
 })
