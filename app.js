9fd97f0f62a2f542e38f034b0969ae8087242a5c
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-06-05 13:38:16 -0400

    added z,t,o,i tetris pieces

--- app.js
+++ app.js
@@ -9,13 +9,38 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartButton = document.querySelector('#start-button');
 
   // making the tetris pieces
-  const lpiece = [
+  const lPiece = [
     [1,width+1,width*2+1,2],
     [width,width+1,width+2,width*2+2],
     [1,width+1,width*2+1,width*2],
     [width, width*2,width*2+1,width*2+2]
    ];
   
-  
+  const zPiece = [
+    [width*2,width+1,width*2+1,width+2],
+    [0,width,width+1,width*2+1],
+    [width*2,width+1,width*2+1,width+2],
+    [0,width,width+1,width*2+1]
+   ];
 
+  const tPiece = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+   ];
+  
+  const oPiece = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+   ];
+  
+  const iPiece = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ];
 })
