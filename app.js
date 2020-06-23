5edd56a6288bf6970560f25683484fc6b5a174f8
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-06-23 14:09:14 -0400

    added addScore() - does not work, broke game

--- app.js
+++ app.js
@@ -188,5 +188,20 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
 
+      // remove row when full, move other pieces down,
+    //increase score
+      function addScore(){
+        for( int i = 0; i < 199; i += width){
+          const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8,i+9]
+          if (row.every(index => squares[index].classList.contains('taken'))){
+            score += 10;
+            ScoreDisplay.innerHTML = score;
+            row.forEach ( index => {
+              squares[index].classList.remove('taken');
+            })
+            const squaresRemoved = squares.splice(i,width);
+          }
+        }
+      }
     
 })
